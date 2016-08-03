76d46b8c405381c31010b43078ea181c30df813c
Steven Pastrana <pastrana.steven.az@gmail.com>
2016-08-03 12:51:45 -0700

    added font awesome icons to the footer

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,22 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$( document ).on('turbolinks:load', function(){
-  
- 
-// Add hover effects to nav bar. Specifically adds 
-// the active class to the <li>
+$(document).on('turbolinks:load', function () {
+
+
+  // Add hover effects to nav bar. Specifically adds 
+  // the active class to the <li>
   $('#about-link').on({
-    mouseenter : function(){
+    mouseenter: function () {
       $(this).addClass('active');
     },
-    mouseleave : function(){
+    mouseleave: function () {
       $(this).removeClass('active');
     }
   })
 
-  $('h1').hover(function(){
-    
+  $('h1').hover(function () {
+
   })
 });
 
