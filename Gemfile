93a6d7b38179377ab82777e67942d0a572770f27
Steven Antonio <pastrana.steven.az@gmail.com>
2016-09-25 19:20:01 -0700

    added sendgrid gem and ssh

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'guard-bundler', group: :development
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
 gem 'devise'
-
+gem 'sendgrid-ruby'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', :group => :development
 gem 'pg', :group => :production
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,16 +58,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
 
-config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => "gmail.com",
-  :authentication => :login,
-  :user_name => "yourdevssolution@gmail.com",
-  :password => "AMerica12",
-}
+#config.action_mailer.smtp_settings = {
+#  :enable_starttls_auto => true,
+#  :address => "smtp.gmail.com",
+#  :port => 587,
+#  :domain => "gmail.com",
+#  :authentication => :login,
+#  :user_name => "yourdevssolution@gmail.com",
+#  :password => "AMerica12",
+#}
 
+#config.action_mailer.default_url_options = {host: }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
